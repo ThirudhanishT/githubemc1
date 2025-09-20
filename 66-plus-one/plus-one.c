33f10027cd8f473628364a63ff517c0b30c2e13d
THIRUDHANISH T <thirudhanish5@gmail.com>
2025-09-20 19:20:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (53.31%) - LeetSync

---
+++ 66-plus-one/plus-one.c
@@ -0,0 +1,24 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+      int* result = (int*)malloc((digitsSize + 1) * sizeof(int));
+      int carry=1;
+      for(int i=digitsSize-1;i>=0;i--)
+      {
+        int sum=digits[i]+carry;
+        result[i+1]=sum%10;
+        carry=sum/10;
+      }
+      if(carry)
+      {
+        result[0]=1;
+        *returnSize=digitsSize+1;
+        return result;
+      }
+      else
+      {
+        *returnSize=digitsSize;
+        return result+1;
+      }
+}
