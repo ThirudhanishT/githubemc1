1b67ab52d1734a55a25bdb888740f5bb911f136f
THIRUDHANISH T <thirudhanish5@gmail.com>
2026-08-04 21:36:50 +0530

    Time: 17 ms (81.27%) | Memory: 49.7 MB (36.46%) - LeetSync

---
+++ 561-array-partition/array-partition.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int a=nums.length;
+        int sum=0;
+        for(int i=0;i<a;i+=2)
+        {
+            sum+=nums[i];
+        }
+        return sum;
+
+    }
+}
