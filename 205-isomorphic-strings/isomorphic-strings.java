0042622f80692baa2b60c5a8d2301065828a3fee
THIRUDHANISH T <thirudhanish5@gmail.com>
2026-09-07 17:52:44 +0530

    Time: 7 ms (78.16%) | Memory: 43.6 MB (86.37%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int map1[]=new int[256];
+        int map2[]=new int[256];
+        Arrays.fill(map1,-1);
+        Arrays.fill(map2,-1);
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            int c1=s.charAt(i);
+            int c2=t.charAt(i);
+            if(map1[c1]!=-1 && map1[c1]!=c2)
+            {
+                return false;
+            }
+            if(map2[c2]!=-1 && map2[c2]!=c1)
+            {
+                return false;
+            }
+            map1[c1]=c2;
+            map2[c2]=c1;
+        }
+        return true;
+    }
+
+}
